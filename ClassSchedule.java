4b0d948dce0d00a6ebdd24a39ec36a1c001eeda5
Ansab9588 <77882239+Ansab9588@users.noreply.github.com>
2024-07-20 19:29:46 +0530

    Add files via upload

---
+++ ClassSchedule.java
@@ -0,0 +1,50 @@
+public class ClassSchedule {
+    private int scheduleId;
+    private String className;
+    private int trainerId;
+    private String dayOfWeek;
+    private String startTime;
+    private String endTime;
+
+
+    
+    public int getScheduleId() {
+        return scheduleId;
+    }
+    public void setScheduleId(int scheduleId) {
+        this.scheduleId = scheduleId;
+    }
+    public String getClassName() {
+        return className;
+    }
+    public void setClassName(String className) {
+        this.className = className;
+    }
+    public int getTrainerId() {
+        return trainerId;
+    }
+    public void setTrainerId(int trainerId) {
+        this.trainerId = trainerId;
+    }
+    public String getDayOfWeek() {
+        return dayOfWeek;
+    }
+    public void setDayOfWeek(String dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+    public String getStartTime() {
+        return startTime;
+    }
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+    public String getEndTime() {
+        return endTime;
+    }
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+
+    
+}
